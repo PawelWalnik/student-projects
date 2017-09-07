496cb73908bd981b132e9ea2c2d29c1c633fb160
Pawel-Komputer\Pawel <pawel.walnik@gmail.com>
2017-09-07 13:32:00 +0200

    no message

---
+++ Projekt3/Projekt3/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Projekt3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.erosion = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.erosionValue = new System.Windows.Forms.TrackBar();
+            this.swToOriginal = new System.Windows.Forms.Button();
+            this.binarization = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.erosionValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.binarization)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1902, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(117, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.06349F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 1590F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 28);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1902, 1005);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(315, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1584, 999);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.erosion);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.erosionValue);
+            this.groupBox1.Controls.Add(this.swToOriginal);
+            this.groupBox1.Controls.Add(this.binarization);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(306, 999);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // erosion
+            // 
+            this.erosion.Location = new System.Drawing.Point(9, 202);
+            this.erosion.Name = "erosion";
+            this.erosion.Size = new System.Drawing.Size(291, 33);
+            this.erosion.TabIndex = 5;
+            this.erosion.Text = "Erosion!";
+            this.erosion.UseVisualStyleBackColor = true;
+            this.erosion.Click += new System.EventHandler(this.erosion_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Level of erosion:";
+            // 
+            // erosionValue
+            // 
+            this.erosionValue.Location = new System.Drawing.Point(9, 140);
+            this.erosionValue.Minimum = 1;
+            this.erosionValue.Name = "erosionValue";
+            this.erosionValue.Size = new System.Drawing.Size(291, 56);
+            this.erosionValue.TabIndex = 3;
+            this.erosionValue.Value = 1;
+            // 
+            // swToOriginal
+            // 
+            this.swToOriginal.Location = new System.Drawing.Point(9, 307);
+            this.swToOriginal.Name = "swToOriginal";
+            this.swToOriginal.Size = new System.Drawing.Size(291, 34);
+            this.swToOriginal.TabIndex = 2;
+            this.swToOriginal.Text = "Switch to original";
+            this.swToOriginal.UseVisualStyleBackColor = true;
+            this.swToOriginal.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // binarization
+            // 
+            this.binarization.Location = new System.Drawing.Point(9, 50);
+            this.binarization.Maximum = 255;
+            this.binarization.Name = "binarization";
+            this.binarization.Size = new System.Drawing.Size(291, 56);
+            this.binarization.TabIndex = 1;
+            this.binarization.MouseCaptureChanged += new System.EventHandler(this.binarization_MouseCaptureChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Binarization:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Erosion";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.erosionValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.binarization)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar binarization;
+        private System.Windows.Forms.Button swToOriginal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar erosionValue;
+        private System.Windows.Forms.Button erosion;
+    }
+}
+
---
+++ Projekt3/Projekt3/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt3
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog selectFile = new OpenFileDialog();
+        private SaveFileDialog saveFile = new SaveFileDialog();
+        private Bitmap bmp;
+        private Bitmap originalBmp;
+        private BitmapData bmpData = null;
+        private Rectangle area;
+        IntPtr dataBegin;
+        int numberOfBytes;
+        byte[] RGBvalues;
+        bool erosionEnabled = false;
+        [DllImport("asm.DLL")]
+        public static extern unsafe void binar(IntPtr Rval, IntPtr Gval, IntPtr Bval, int treshold, int lenght);
+        //[DllImport("asm.DLL")]
+        //public static extern unsafe void eros(IntPtr matrices, IntPtr RGBvalues, int lenght, int mLenght);
+
+        public Form1()
+        {
+            InitializeComponent();
+            selectFile.Title = "Select image to open";
+            selectFile.Filter = "Image File (*.jpg, *.gif, *.png, *.bmp) |";
+            saveFile.Title = "Save picture";
+            saveFile.Filter = "JPEG File|*.jpg|GIF File|*.gif|PNG File|*.png|BMP File|*.bmp";
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (selectFile.ShowDialog() == DialogResult.OK && selectFile.OpenFile() != null)
+            {
+                bmp = Bitmap.FromFile(selectFile.FileName) as Bitmap;
+                originalBmp = new Bitmap(bmp);
+                pictureBox.Image = bmp;
+                pictureBox.Width = bmp.Width;
+                pictureBox.Height = bmp.Height;
+                area = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                numberOfBytes = bmp.Width * bmp.Height * 3;
+                RGBvalues = new byte[numberOfBytes];
+                binarization.Value = 0;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void binarization_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            if (bmp != null)
+            {
+                int treshold = binarization.Value;
+
+                bmpData = bmp.LockBits(area, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+                dataBegin = bmpData.Scan0;
+                Marshal.Copy(dataBegin, RGBvalues, 0, numberOfBytes);
+                bmp.UnlockBits(bmpData);
+
+                // Separation of RGB channels
+                byte[] Rvalues = new byte[numberOfBytes / 3];
+                byte[] Gvalues = new byte[numberOfBytes / 3];
+                byte[] Bvalues = new byte[numberOfBytes / 3];
+                for (int i = 0; i < numberOfBytes; i = i + 3)
+                {
+                    Rvalues[i / 3] = RGBvalues[i];
+                    Gvalues[i / 3] = RGBvalues[i + 1];
+                    Bvalues[i / 3] = RGBvalues[i + 2];
+                }
+
+                // Declaration of an unmanaged data segment
+                IntPtr Rvalues_ = Marshal.AllocHGlobal(Rvalues.Length * sizeof(byte));
+                IntPtr Gvalues_ = Marshal.AllocHGlobal(Gvalues.Length * sizeof(byte));
+                IntPtr Bvalues_ = Marshal.AllocHGlobal(Bvalues.Length * sizeof(byte));
+                Marshal.Copy(Rvalues, 0, Rvalues_, numberOfBytes / 3);
+                Marshal.Copy(Gvalues, 0, Gvalues_, numberOfBytes / 3);
+                Marshal.Copy(Bvalues, 0, Bvalues_, numberOfBytes / 3);
+
+
+                // DLL support (binarization)
+                binar(Rvalues_, Gvalues_, Bvalues_, treshold, numberOfBytes / 3);
+
+                // Restore data from an unmanaged data segment
+                Marshal.Copy(Rvalues_, Rvalues, 0, numberOfBytes / 3);
+                Marshal.Copy(Gvalues_, Gvalues, 0, numberOfBytes / 3);
+                Marshal.Copy(Bvalues_, Bvalues, 0, numberOfBytes / 3);
+                Marshal.FreeHGlobal(Rvalues_);
+                Marshal.FreeHGlobal(Gvalues_);
+                Marshal.FreeHGlobal(Bvalues_);
+
+                // Connecting RGB channels
+                for (int i = 0; i < numberOfBytes; i = i + 3)
+                {
+                    RGBvalues[i] = Rvalues[i / 3];
+                    RGBvalues[i + 1] = Gvalues[i / 3];
+                    RGBvalues[i + 2] = Bvalues[i / 3];
+                }
+
+
+                bmpData = bmp.LockBits(area, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+                dataBegin = bmpData.Scan0;
+                Marshal.Copy(RGBvalues, 0, dataBegin, numberOfBytes);
+                bmp.UnlockBits(bmpData);
+
+                pictureBox.Image = bmp;
+                erosionEnabled = true;
+            }
+            else
+                binarization.Value = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(bmp != null)
+            {
+                pictureBox.Image = originalBmp;
+                bmp = new Bitmap(originalBmp);
+                binarization.Value = 0;
+                erosionEnabled = false;
+                erosionValue.Value = 1;
+            }
+        }
+
+        private void erosion_Click(object sender, EventArgs e)
+        {
+            if (erosionEnabled)
+            {
+                bmpData = bmp.LockBits(area, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+                dataBegin = bmpData.Scan0;
+                byte[] RGBValuesCopy = new byte[numberOfBytes];
+
+
+
+                int RGBWidth = bmp.Width * 3;
+
+                Marshal.Copy(dataBegin, RGBvalues, 0, numberOfBytes);
+                for (int j = 1; j <= erosionValue.Value; j++)
+                {
+                    for (int i = 0; i < RGBvalues.Length; i++)
+                    {
+                        RGBValuesCopy[i] = RGBvalues[i];
+                    }
+
+                    //middle
+                    for (int i = RGBWidth + 3; i < RGBvalues.Length - RGBWidth - 3; i += 3)
+                    {
+                        if (((i + 3) % RGBWidth) == 0)
+                        {
+                            i += 3;
+                            continue;
+                        }
+                        if (RGBvalues[i + 3] == 255 || RGBvalues[i - 3] == 255 || RGBvalues[i - RGBWidth] == 255 || RGBvalues[i - RGBWidth - 3] == 255 || RGBvalues[i - RGBWidth + 3] == 255 || RGBvalues[i + RGBWidth] == 255 || RGBvalues[i + RGBWidth - 3] == 255 || RGBvalues[i + RGBWidth + 3] == 255)
+                        {
+                            RGBValuesCopy[i] = 255;
+                            RGBValuesCopy[i + 1] = 255;
+                            RGBValuesCopy[i + 2] = 255;
+                        }
+
+                    }
+                    //upper edge
+                    for (int i = 4; i < RGBWidth - 3; i += 3)
+                    {
+                        if (RGBvalues[i + 3] == 255 || RGBvalues[i - 3] == 255 || RGBvalues[i + RGBWidth] == 255 || RGBvalues[i + RGBWidth - 3] == 255 || RGBvalues[i + RGBWidth + 3] == 255)
+                        {
+                            RGBValuesCopy[i] = 255;
+                            RGBValuesCopy[i + 1] = 255;
+                            RGBValuesCopy[i + 2] = 255;
+                        }
+                    }
+                    //bottom edge
+                    for (int i = RGBvalues.Length - RGBWidth + 3; i < RGBvalues.Length - 3; i += 3)
+                    {
+                        if (RGBvalues[i + 3] == 255 || RGBvalues[i - 3] == 255 || RGBvalues[i - RGBWidth] == 255 || RGBvalues[i - RGBWidth - 3] == 255 || RGBvalues[i - RGBWidth + 3] == 255)
+                        {
+                            RGBValuesCopy[i] = 255;
+                            RGBValuesCopy[i + 1] = 255;
+                            RGBValuesCopy[i + 2] = 255;
+                        }
+                    }
+                    //left edge
+                    for (int i = RGBWidth; i < RGBvalues.Length - RGBWidth; i += RGBWidth)
+                    {
+                        if (RGBvalues[i + 3] == 255 || RGBvalues[i - RGBWidth] == 255 || RGBvalues[i - RGBWidth + 3] == 255 || RGBvalues[i + RGBWidth] == 255 || RGBvalues[i + RGBWidth + 3] == 255)
+                        {
+                            RGBValuesCopy[i] = 255;
+                            RGBValuesCopy[i + 1] = 255;
+                            RGBValuesCopy[i + 2] = 255;
+                        }
+                    }
+                    //right edge
+                    for (int i = 2*RGBWidth - 3; i < RGBvalues.Length - RGBWidth; i += RGBWidth)
+                    {
+                        if (RGBvalues[i - 3] == 255 || RGBvalues[i - RGBWidth] == 255 || RGBvalues[i - RGBWidth - 3] == 255 || RGBvalues[i + RGBWidth] == 255 || RGBvalues[i + RGBWidth - 3] == 255)
+                        {
+                            RGBValuesCopy[i] = 255;
+                            RGBValuesCopy[i + 1] = 255;
+                            RGBValuesCopy[i + 2] = 255;
+                        }
+                    }
+                    int s = 0;
+                    //upper left corner
+                    if (RGBvalues[s + 3] == 255|| RGBvalues[s + RGBWidth] == 255 || RGBvalues[s + RGBWidth + 3] == 255)
+                    {
+                        RGBValuesCopy[s] = 255;
+                        RGBValuesCopy[s + 1] = 255;
+                        RGBValuesCopy[s + 2] = 255;
+                    }
+                    s = RGBWidth - 3;
+                    //uper right corner
+                    if (RGBvalues[s - 3] == 255 || RGBvalues[s + RGBWidth - 3] == 255 || RGBvalues[s + RGBWidth] == 255)
+                    {
+                        RGBValuesCopy[s] = 255;
+                        RGBValuesCopy[s + 1] = 255;
+                        RGBValuesCopy[s + 2] = 255;
+                    }
+                    //bottom left corner
+                    s = RGBvalues.Length - RGBWidth;
+                    if (RGBvalues[s + 3] == 255 || RGBvalues[s - RGBWidth] == 255 || RGBvalues[s - RGBWidth + 3] == 255)
+                    {
+                        RGBValuesCopy[s] = 255;
+                        RGBValuesCopy[s + 1] = 255;
+                        RGBValuesCopy[s + 2] = 255;
+                    }
+                    //bottom right corner
+                    s = RGBvalues.Length - 3;
+                    if (RGBvalues[s - 3] == 255 || RGBvalues[s - RGBWidth] == 255 || RGBvalues[s - RGBWidth - 3] == 255)
+                    {
+                        RGBValuesCopy[s] = 255;
+                        RGBValuesCopy[s + 1] = 255;
+                        RGBValuesCopy[s + 2] = 255;
+                    }
+
+
+                    for (int i = 0; i < RGBvalues.Length; i++)
+                    {
+                        RGBvalues[i] = RGBValuesCopy[i];
+                    }
+                }
+                Marshal.Copy(RGBvalues, 0, dataBegin, numberOfBytes);
+                bmp.UnlockBits(bmpData);
+
+                pictureBox.Image = bmp;
+
+            }
+            else
+                erosionValue.Value = 1;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (bmp != null)
+            {
+                DialogResult result = saveFile.ShowDialog();
+                if (result == DialogResult.OK && saveFile.FileName != "")
+                {
+                    // Saving image
+                    pictureBox.Image.Save(saveFile.FileName);
+                }
+            }
+        }
+    }
+}
